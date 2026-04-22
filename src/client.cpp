0c1b4955cec522c80636ebd7f618e8d1641a095c
Kevin Ahrendt <kahrendt@gmail.com>
2026-04-22 07:40:16 -0400

    to be consistent, make the artwork stream start handler also receive it's relevant section. The artwork role will just validate that it matches what we expect

--- src/client.cpp
+++ src/client.cpp
@@ -484,7 +484,7 @@ void SendspinClient::process_json_message(SendspinConnection* conn, const std::s
 
 #ifdef SENDSPIN_ENABLE_ARTWORK
             if (this->artwork_ && stream_msg.artwork.has_value()) {
-                this->artwork_->impl_->handle_stream_start();
+                this->artwork_->impl_->handle_stream_start(stream_msg.artwork.value());
             }
 #endif
 
